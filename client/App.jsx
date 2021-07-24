4d49ca11ce106326eb9c734bf57253f3f081b8f2
Hannah Manfredi <hmanfredi@gmail.com>
2021-07-24 11:51:06 -0700

    Get data from CoinDesk API and parse into array I can pass into my Fragment

--- client/App.jsx
+++ client/App.jsx
@@ -1,34 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: []
+      loading: true,
+      data: [],
+      chartData: []
     };
   }
 
   componentDidMount() {
-    console.log('hi')
-    // axios
-    //   .get('https://api.coindesk.com/v1/bpi/historical/close.json')
-    //     .then(res => {
-    //       console.log(res)
-    //       this.setState({
-    //         data: res.data
-    //       });
-    //     })
+    axios
+      .get('https://api.coindesk.com/v1/bpi/historical/close.json')
+        .then(res => {
+          const bpiList = [];
+          for (let key in res.data.bpi) {
+            let item = {
+              date: key,
+              price: res.data.bpi[key]
+            };
+            bpiList.push(item)
+          }
+          this.setState({
+            data: bpiList,
+            loading: !this.state.loading
+          }), () => {
+            console.log('create graph w data')
+          }
+        })
+        .catch((err) => {
+          throw err;
+        });
   }
 
   render() {
-    console.log(this.state.data)
-    return(
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    );
+    if (this.state.loading) {
+      return (
+        <div>
+          <h1>Loading!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>Crypto Charting Tool</h1>
+          <dl>
+            <>
+              {this.state.data.map((item, i) => (
+                <React.Fragment key={i}>
+                  <dt>{item.date}</dt>
+                  <dd>{item.price}</dd>
+                </React.Fragment>
+              ))}
+            </>
+          </dl>
+        </div>
+      );
+    }
   }
 }
 
--- server.js
+++ server.js
@@ -1,26 +1,10 @@
-// const express = require('express')
-// const cors = require('cors')
-// const parser = require('body-parser')
-// const path = require('path')
-
-// const app = express()
-// const port = 3000
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`)
-// })
-
-// module.exports = app;
-
 const express = require('express')
+const parser = require('body-parser')
+const path = require('path')
+
 const app = express()
 const port = 3000
 
-app.use(cors)
 app.use(parser.urlencoded({ extended: false}))
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
@@ -32,3 +16,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+module.exports = app;
